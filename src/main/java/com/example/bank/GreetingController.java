742b51c5cd211fac17804aad219daaa92d2935b2
HHameem <hhameem@my.centennialcollege.ca>
2025-02-24 19:51:11 -0500

    Initial commit of Spring Boot Welcome App

---
+++ src/main/java/com/example/bank/GreetingController.java
@@ -0,0 +1,20 @@
+package com.example.bank;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalTime;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        int hour = LocalTime.now().getHour();
+        String greeting = hour < 12 ? "Good morning" : "Good afternoon";
+        model.addAttribute("greeting", greeting);
+        model.addAttribute("name", "Hafshan Hameem");
+        return "index";
+    }
+}
